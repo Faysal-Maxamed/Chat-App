8aecc9734b206f3cf4b7626cf30ecc058e71c86d
Faysal-Maxamed <pheymoha17@gmail.com>
2025-11-05 11:33:19 +0300

    ..

--- server/routes/MessageRoutes.js
+++ server/routes/MessageRoutes.js
@@ -2,11 +2,12 @@ import express from 'express';
 
 const messageRoutes =express.Router();
 
-import {sendMessage,getMessages,getChatList} from '../controllers/MessageController.js'
+import {sendMessage,getMessages,getChatList, searchUsers} from '../controllers/MessageController.js'
 import { protect } from '../middleware/authmidlleware.js';
 
 messageRoutes.post('/send',protect,sendMessage);
 messageRoutes.get('/:userId',protect,getMessages);
 messageRoutes.get('/chats/list',protect,getChatList);
+messageRoutes.get('search',protect,searchUsers)
 
 export default messageRoutes;
